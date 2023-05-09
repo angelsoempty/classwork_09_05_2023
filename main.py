91e34eb694fddb32a4c935a04c2b87e0da6400df
angelsoempty <denatamanov1@gmail.com>
2023-05-09 19:20:11 +0300

    знайомство з filter()

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class School:
         self.students.append(student)
         print(f'{student.name} Був доданий до школи {self.name}') #дописати, коли створемо клас
 
+    def expel_student(self, student):
+        pass
+
 class Student:
     def __init__(self, name, grade):
         self.name = name
@@ -19,3 +22,9 @@ class Student:
         print(f'{self.name} був понижений {self.grade}')
     def __str__(self):
         return f'{self.name} - Ранг {self.grade}'
+
+multiply = lambda x, y: x * y
+print(multiply(2, 5))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+filtered_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(filtered_numbers)
