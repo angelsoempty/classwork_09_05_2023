8af394211eaac706056ff10bad4154980fccca88
angelsoempty <denatamanov1@gmail.com>
2023-05-09 19:03:26 +0300

    додано метод виводу

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ class School:
         self.students = students #cписок студентів
     def admit_student(self, student): #зарахування студентів
         self.students.append(student)
-        print(f'{}') #дописати, коли створемо клас
+        print(f'{student.name} Був доданий до школи {self.name}') #дописати, коли створемо клас
 
 class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+    def promote(self):
+        self.grade += 1
+        print(f'{self.name} був підвищений {self.grade}')
+    def demote(self):
+        self.grade -= 1
+        print(f'{self.name} був понижений {self.grade}')
+    def __str__(self):
+        return f'{self.name} - Ранг {self.grade}'
